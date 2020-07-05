b1b22a7387e156b2d73bfe11218ac7e0d7c449ef
nizhiyuan2007@126.com <nizhiyuan2007@126.com>
2020-07-05 14:23:23 +0800

    add cart

--- vuejs01/mysite/src/App.vue
+++ vuejs01/mysite/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <h1>购物车</h1>
+    <h1>{{title}}</h1>
     <ul>
         <li v-for="item in cartList" :key="item.id">
             <h3>{{item.title}}</h3>
@@ -9,26 +9,28 @@
         </li>
     </ul>
 
+    <Cart :title="title" :cart="cartList"> </Cart>
   </div>
 </template>
 
 <script>
 
-import MyCart  from "./components/Cart";
+import Cart  from "./components/Cart";
 
 export default {
   name: 'App',
   data(){
       return {
+          title: "购物车",
           cartList: [
-              {id: 1, title: "Vue实战开发", price: 100.00},
-              {id: 2, title: "React实战开发", price: 200.00},
-              {id: 3, title: "Python实战开发", price: 300.00} 
+              {id: 1, title: "Vue实战开发", price: 100.00, active:true, count:1},
+              {id: 2, title: "React实战开发", price: 200.00, active:true, count:1},
+              {id: 3, title: "Python实战开发", price: 300.00, active:true, count:1} 
           ]
       }
   },
   components: {
-        
+       Cart 
   }
 }
 </script>
--- vuejs01/mysite/src/components/Cart.vue
+++ vuejs01/mysite/src/components/Cart.vue
@@ -1,15 +1,40 @@
 <template>
-  
+    <div id="app">
+        <h3>{{ title }}</h3>
+        <table border="1" cellspacing="0">
+            <tr>
+                <th>#</th>
+                <th>课程</th>
+                <th>单价</th>
+                <th>数量</th>
+                <th>总价</th>
+            </tr>
+            <tr v-for="c in cart" :key="c.id">
+                <td><input type="checkbox" v-model="c.active"></td>
+                <td>{{ c.title }}</td>
+                <td>{{ c.price }}</td>
+                <td>
+                    <button>-</button>
+                    {{ c.count }}
+                    <button>+</button>
+                </td>
+                <td>¥{{ c.price * c.count }}</td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <script>
 export default {
-    "name": "Cart"
+    name: "Cart",
+    props: ["title", "cart"]
+
 }
 </script>
 
+
 <style scoped>
-
-</style>>
-
+h3, table {
+    margin: auto;
+}
 </style>
